f0ce6adbe82e76887d2971a3086afb12256ba38b
damienbod <damien_bod@hotmail.com>
2024-05-23 20:42:27 +0200

    Add some demo data

--- ApiCreatePdf/DocumentService.cs
+++ ApiCreatePdf/DocumentService.cs
@@ -38,11 +38,30 @@ public class DocumentService
         var section = new Section(document);
         document.Sections.Add(section);
 
+        // Main text 
         var paragraph = new Paragraph(document);
         section.Blocks.Add(paragraph);
-
         var run = new Run(document, documentData.MainContentText);
         paragraph.Inlines.Add(run);
+
+        var bookmarkName = "TopOfDocument";
+
+        document.Sections.Add(
+            new Section(document,
+                new Paragraph(document,
+                    new BookmarkStart(document, bookmarkName),
+                    new Run(document, "This is a 'TopOfDocument' bookmark."),
+                    new BookmarkEnd(document, bookmarkName)),
+                new Paragraph(document,
+                    new Run(document, "The following is a link to "),
+                    new Hyperlink(document, "https://www.gemboxsoftware.com/document", "GemBox.Document Overview"),
+                    new Run(document, " page.")),
+                 new Paragraph(document,
+                    new SpecialCharacter(document, SpecialCharacterType.PageBreak),
+                    new Run(document, "This is a document's second page."),
+                    new SpecialCharacter(document, SpecialCharacterType.LineBreak),
+                    new Hyperlink(document, bookmarkName, "Return to 'TopOfDocument'.") { IsBookmarkLink = true })));
+
         return document;
     }
 
